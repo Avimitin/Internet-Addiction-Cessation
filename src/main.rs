676692d34c7803d9a8e3d8017869675d397a6d4e
Avimitin <avimitin@gmail.com>
2021-10-25 16:49:23 +0800

    io: wrap read file into a function
    Create a function read_file_into_string(&str). It accept string literal
    as file path. And return file contents as String struct if it
    successfully read the file.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{BufReader, BufRead};
 
 fn main() {
-    let path = "hosts.txt";
+    let path = "./fixtures/hosts.txt";
+    println!("{}", read_file_into_string(path).unwrap());
+}
 
-    let file = File::create(path);
-    let mut file = match file {
+fn read_file_into_string(file_path: &'static str) -> Result<String, String> {
+    let mut content = String::new();
+
+    let file = File::open(file_path);
+    let file = match file {
         Ok(f) => f,
-        Err(e) => panic!("Fail to read {path}: {error}", path=path, error=e),
+        Err(e) => return Err(format!("Open file '{path}': {error}", path=file_path, error=e)),
     };
 
-    if let Err(e) = file.write_all(b"Hosts file\n") {
-        panic!("Write file {}: {}", path, e)
-    };
+    let buffer = BufReader::new(file);
+    for line in buffer.lines() {
+        if let Ok(line) = line {
+            content += &line;
+        }
+    }
+
+    Ok(content)
 }
