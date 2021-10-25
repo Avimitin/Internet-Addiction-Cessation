2b126a391ca636a8eeccbfd42b55b3a7e6a1da11
Avimitin <avimitin@gmail.com>
2021-10-25 22:19:09 +0800

    crate: create new mod host_file
    The module file is a macroscopic name. This mean it should not only
    store implementation of the host file. So here I split the host file I/O
    implementation to the submodule host_file. The file module can store
    configuration file or other file's implementation.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use auto_domain_blocker::file;
+use auto_domain_blocker::file::host_file;
 
 fn main() {
     let path = "./fixtures/hosts.txt";
-    let host_file = file::HostFile::new(path).unwrap();
+    let host_file = host_file::HostFile::new(path).unwrap();
     println!("File: {}\n{}", host_file.which(), host_file.cat());
 }
 
