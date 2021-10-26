38b33f5e1aa957e4193eb8bfaed7c7a7e20a5ac1
Avimitin <avimitin@gmail.com>
2021-10-26 22:20:29 +0800

    fixtures: design project specific file format
    It is important to keep the contents of the original host file. So I
    use XML format to identify auto-blocker's content and the original
    content.
    
    Domain inside "## <!--auto domain blocker -->" are managed by this
    program. Other text will keep at the same places.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- tests/file_test.rs
+++ tests/file_test.rs
@@ -1,12 +1,11 @@
 use auto_domain_blocker::file::*;
 
 const HOST_FILE_PATH: &str = "./fixtures/hosts.txt";
-const HOST_FILE_CONTENT: &str = "0.0.0.0 bilibili.com\n";
 
 #[test]
 fn test_host_file() {
     let f = host_file::HostFile::new(HOST_FILE_PATH).unwrap();
 
-    assert_eq!(f.cat(), HOST_FILE_CONTENT);
+    assert!(f.cat().len() != 0);
     assert_eq!(f.which(), HOST_FILE_PATH);
 }
