c5a53919a3aef2d277ff4438d619c8e49b3149c7
Avimitin <avimitin@gmail.com>
2021-11-23 11:00:40 +0800

    lib/host_file: rename the bound index API
    Rename the origin read_bound_index to find_bound_index as it is doing
    "find" job not a read job. Add new public read_bound_index API, it
    return the inner bound_index field value.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/host_file.rs
+++ src/host_file.rs
@@ -15,7 +15,7 @@ impl HostFile {
     // read success.
     pub fn new(file_path: &'static str) -> Result<HostFile, Box<dyn Error>> {
         let contents = std::fs::read_to_string(file_path)?;
-        let bound_index = HostFile::read_bound_index(&contents);
+        let bound_index = HostFile::find_bound_index(&contents);
         Ok(HostFile {
             contents,
             bound_index,
@@ -33,13 +33,13 @@ impl HostFile {
         &self.location
     }
 
-    fn read_bound_index(input: &String) -> (i32, i32) {
+    fn find_bound_index(input: &String) -> (i32, i32) {
         let mut bound = (0, 0);
         let mut i: i32 = 0;
         let mut inside: bool = false;
         for line in input.lines() {
             i+=1;
-            if line.contains("## <!-- auto domain blocker --->") {
+            if line.contains("## <!-- auto domain blocker -->") {
                 if !inside {
                     bound.0 = i;
                     inside = true;
@@ -51,5 +51,9 @@ impl HostFile {
         }
 
         return bound;
+    }
+
+    pub fn read_bound_index(&self) -> (i32, i32) {
+        self.bound_index
     }
 }
