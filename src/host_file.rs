521f6b11e27f2c676ae71253f185935c2dbe273f
Avimitin <avimitin@gmail.com>
2021-11-23 10:49:27 +0800

    lib/host_file: add bound index field and simplify.. ..the code
    This commit rewrite the HostFile::new function. Use
    std::fs::read_to_string to replace the whole multiple match. This can
    help improve the code readbility.
    
    Also this commit implement the read_bound_index API. This API can help
    finding generated contents and easily delete them.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/host_file.rs
+++ src/host_file.rs
@@ -4,28 +4,21 @@ pub struct HostFile {
     location: String,
     // contents contain buffer of the host file
     contents: String,
+    // bound_index store the start and end bound of the generated contents
+    bound_index: (i32, i32),
 }
 
+#[allow(dead_code)]
+use std::error::Error;
 impl HostFile {
     // new read conents from given file path and return a HostFile instance if
     // read success.
-    pub fn new(file_path: &'static str) -> Result<HostFile, String> {
-        let contents = std::fs::read(file_path);
-        let contents = match contents {
-            Ok(contents) => match String::from_utf8(contents) {
-                Ok(buffer) => buffer,
-                Err(err) => {
-                    return Err(format!(
-                        "File {} contains invalid contents: {}",
-                        file_path, err
-                    ))
-                }
-            },
-            Err(err) => return Err(format!("Read file '{}': {:?}'", file_path, err)),
-        };
-
+    pub fn new(file_path: &'static str) -> Result<HostFile, Box<dyn Error>> {
+        let contents = std::fs::read_to_string(file_path)?;
+        let bound_index = HostFile::read_bound_index(&contents);
         Ok(HostFile {
             contents,
+            bound_index,
             location: file_path.to_string(),
         })
     }
@@ -38,5 +31,25 @@ impl HostFile {
     // which return the file path of this host file
     pub fn which(&self) -> &str {
         &self.location
+    }
+
+    fn read_bound_index(input: &String) -> (i32, i32) {
+        let mut bound = (0, 0);
+        let mut i: i32 = 0;
+        let mut inside: bool = false;
+        for line in input.lines() {
+            i+=1;
+            if line.contains("## <!-- auto domain blocker --->") {
+                if !inside {
+                    bound.0 = i;
+                    inside = true;
+                } else {
+                    bound.1 = i;
+                    return bound;
+                }
+            }
+        }
+
+        return bound;
     }
 }
