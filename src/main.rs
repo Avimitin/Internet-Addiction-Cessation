6e19fa346ae3a840711fa78d508d6d411562c2bd
Avimitin <avimitin@gmail.com>
2021-11-24 12:32:37 +0800

    [WIP] cli: add run logic
    * Move CLI assignment to individual function
    * Add option `--config`
    * Add subcommand handler
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
-use clap::App;
+use clap::{App, ArgMatches, Arg};
+use anyhow::{Context, Result};
+use auto_domain_blocker::{config, host_file};
 
-fn main() {
-    let app = App::new("Auto domains blocker")
+fn main() -> Result<()> {
+    let app = build_cli_app();
+    let path = app.value_of("config").unwrap_or("./domains.toml");
+    let cfg = config::Config::new(path)?;
+
+    run(&app, &cfg)
+        .with_context(|| {
+            format!("Run app with config: {} fail", path)
+        })?;
+
+    Ok(())
+}
+
+fn run(app: &ArgMatches, cfg: &config::Config) -> Result<()> {
+    if app.is_present("block") {
+        println!("Running block process");
+        let mut hf = host_file::HostFile::new("/etc/hosts")?;
+
+        hf.generate(cfg)?;
+    }
+
+    if app.is_present("unblock") {
+        println!("Running unblock process");
+    }
+
+    Ok(())
+}
+
+fn build_cli_app() -> ArgMatches {
+    App::new("Auto domains blocker")
         .version("0.1")
         .author("Avimitin <avimitin@gmail.com>")
         .about("This app help you get rid of internet addiction")
-        .subcommand(App::new("block")
-            .about("block all the domains now"))
-        .get_matches();
-
-    if let Some(_) = app.subcommand_matches("block") {
-        println!("Running block process");
-    }
+        .subcommands(vec![
+            App::new("block")
+                .about("Block all the domains now when it is time to study"),
+            App::new("unblock")
+                .about("Unblock all the domains only when it is time to relax"),
+        ])
+        .arg(Arg::new("config")
+            .short('c')
+            .long("config")
+            .value_name("CONFIG_PATH")
+            .about("Set the path to the user specific config file")
+            .takes_value(true))
+        .get_matches()
 }
-
